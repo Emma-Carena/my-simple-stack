62dfced3a8d6ccaed3669f8e636275c087df42d4
Emma-Carena <emma_carena@orange.fr>
2019-10-24 08:54:41 +0200

    Ajout des classes et de leurs tests + modification du pom

---
+++ src/main/java/deqo/emau/mysimplestack/SimpleStackClass.java
@@ -0,0 +1,34 @@
+package deqo.emau.mysimplestack;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class SimpleStackClass implements SimpleStack{
+
+    private Stack<Object> wrappedStack = new Stack<>();
+
+    @Override
+    public boolean isEmpty() {
+        return wrappedStack.isEmpty();
+    }
+
+    @Override
+    public int getSize() {
+        return wrappedStack.size();
+    }
+
+    @Override
+    public void push(Item item) {
+        wrappedStack.push(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        return (Item) wrappedStack.peek();
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        return (Item) wrappedStack.pop();
+    }
+}
