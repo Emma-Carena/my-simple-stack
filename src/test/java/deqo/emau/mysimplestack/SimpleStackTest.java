1c62983d0ab5fd1dcb49448a53a0f416e94eec03
Emma-Carena <emma_carena@orange.fr>
2019-10-24 14:47:43 +0200

    Refactorisation des tests

--- src/test/java/deqo/emau/mysimplestack/SimpleStackTest.java
+++ src/test/java/deqo/emau/mysimplestack/SimpleStackTest.java
@@ -4,11 +4,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.EmptyStackException;
-import java.util.Stack;
-
-import static org.junit.Assert.*;
-
 public class SimpleStackTest {
 
     SimpleStack simpleStack;
@@ -16,28 +11,71 @@ public class SimpleStackTest {
     @Before
     public void setUp() throws Exception {
         simpleStack = new SimpleStackClass();
-        System.out.println("Je suis exécuté avant chaque test");
+        System.out.println("Je suis exécuté avant chaque test.\n");
     }
 
     @Test
-    public void testCreateEmptyStack() {
-        simpleStack = new SimpleStackClass();
-        assertTrue("A new stack should be empty", simpleStack.isEmpty());
-        assertEquals("A new stack has no element", 0, simpleStack.getSize());
+    public void isEmpty() throws Exception {
+        Assert.assertEquals(true, simpleStack.isEmpty());
+        java.lang.String element1 = "toto";
+        simpleStack.push(new Item(new String(element1)));
+        Assert.assertEquals(false, simpleStack.isEmpty());
     }
 
     @Test
-    public void testPush() throws EmptyStackException {
-        SimpleStack simpleStack = new SimpleStackClass();
-        Item item = new Item("toto");
-        simpleStack.push(item);
-        assertFalse("The stack must not be empty", simpleStack.isEmpty());
-        assertEquals("Pushed item should be on top of the stack", item, simpleStack.peek());
+    public void getSize() throws Exception {
+        Assert.assertEquals(0, simpleStack.getSize());
+        String element1 = "toto";
+        simpleStack.push(new Item(new String(element1)));
+        simpleStack.push(new Item(new String(element1+"2")));
+        // La taille doit augmenter de 2
+        Assert.assertEquals(2, simpleStack.getSize());
     }
 
-    @Test(expected = EmptyStackException.class)
-    public void testPopOnEmptyStack() {
-        SimpleStack simpleStack = new SimpleStackClass();
-        simpleStack.pop();
+    @Test
+    public void push() throws Exception {
+        Item ita = new Item(new Integer(8));
+        Assert.assertEquals(0, simpleStack.getSize());
+        simpleStack.push(ita);
+        // La pile doit etre augmentee de 1 item
+        Assert.assertEquals(1, simpleStack.getSize());
+        Item o = simpleStack.peek();
+        // On doit retrouver l'objet initial
+        Assert.assertTrue(o.getValue() instanceof Integer);
+        Integer integer = (Integer)o.getValue();
+        Assert.assertEquals(8, integer.intValue());
+        Assert.assertEquals(ita.getValue(), integer);
+    }
+
+    @Test
+    public void peek() throws Exception {
+        Item ita = new Item(new Integer(8));
+        Assert.assertEquals(0, simpleStack.getSize());
+        simpleStack.push(ita);
+        Assert.assertEquals(1, simpleStack.getSize());
+        Item o = simpleStack.peek();
+        // On ne doit pas retirer l'objet de la pile
+        Assert.assertEquals(1, simpleStack.getSize());
+        // On doit retrouver l'objet initial
+        Assert.assertTrue(o.getValue() instanceof Integer);
+        Integer integer = (Integer)o.getValue();
+        Assert.assertEquals(8, integer.intValue());
+        Assert.assertEquals(ita.getValue(), integer);
+    }
+
+    @Test
+    public void pop() throws Exception {
+        Item ita = new Item(new Integer(8));
+        Assert.assertEquals(0, simpleStack.getSize());
+        simpleStack.push(ita);
+        Assert.assertEquals(1, simpleStack.getSize());
+        Item o = simpleStack.pop();
+        // On doit retirer l'objet de la pile
+        Assert.assertEquals(0, simpleStack.getSize());
+        // On doit retrouver l'objet initial
+        Assert.assertTrue(o.getValue() instanceof Integer);
+        Integer integer = (Integer)o.getValue();
+        Assert.assertEquals(8, integer.intValue());
+        Assert.assertEquals(ita.getValue(), integer);
     }
 }
